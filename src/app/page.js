bd33335e140c40de3144bdd4f26592c5e6c8bf65
HarikrushnR <harikrushnr.codetown@gmail.com>
2023-04-25 11:04:13 +0530

    change

--- src/app/page.js
+++ src/app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
             <p className='font-bold text-xl'>Boris McCranie</p>
           </div>
 
-          <div className='bg-[#faf8ff] w-full  my-5 flex items-center justify-around px-2  md:px-6 lg:px-10 py-4'>
+          <div className='bg-[#faf8ff] w-11/12 mx-auto rounded-lg  my-5 flex items-center justify-around px-2  md:px-6 lg:px-10 py-4'>
             <div className='flex flex-col items-center justify-center'>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#f8a418" className="w-6 h-6">
                 <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clipRule="evenodd" />
